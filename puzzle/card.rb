e9d54484f6b70d634c6b91e358ceaf4aa8850e21
Minh Nguyen <m9nguyen@ucsd.edu>
2021-07-20 14:12:29 -0700

    first  switch

---
+++ puzzle/card.rb
@@ -0,0 +1,34 @@
+class Card 
+    def initialize(mark)
+        @mark = mark
+        @face_up = false  
+    end
+
+    def face_up?
+        @face_up
+    end
+
+    def hide
+        @face_up = false if face_up?
+    end
+
+    def reveal
+        @face_up = true if face_up? == false 
+    end
+
+    def display
+        @mark if face_up? 
+    end
+
+    def to_s
+    end
+
+    def ==(card)
+        if self.mark == card.mark 
+            return true 
+        else  
+            return false 
+        end
+    end
+
+end
